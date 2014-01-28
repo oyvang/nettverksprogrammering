a650f54465996fa88c3a4084dd45d77ebdf11b81
unknown <gm_larsen@oyvang.org>
2014-01-28 18:00:31 +0100

    Finpuss

--- oving1/PrimeMain.java
+++ oving1/PrimeMain.java
@@ -26,30 +26,42 @@ public class PrimeMain {
     }
     public static void main(String[] args) {
         int from = 1;
-        int to = 10000000;
+        int to = 10000;
+        int threads = 4;
+        ArrayList<Prime> p = new ArrayList<>();
 
-        Prime p1 = new Prime(from, to/4);
-        Prime p2 = new Prime(to/4+1, to/3);
-        Prime p3 = new Prime(to/3+1, to/2);
-        Prime p4 = new Prime(to/2+1, to);
+        for (int i = threads; i >0; i--) {
+            p.add(new Prime(from, to/i));
+            from = (to/i) +1;
+        }
 
-        p1.start();
-        p2.start();
-        p3.start();
-        p4.start();
+//        Prime p1 = new Prime(from, to/4);
+//        Prime p2 = new Prime(to/4+1, to/3);
+//        Prime p3 = new Prime(to/3+1, to/2);
+//        Prime p4 = new Prime(to/2+1, to);
+        for (int i = 0; i <threads ; i++) {
+            p.get(i).start();
+        }
+//        p1.start();
+//        p2.start();
+//        p3.start();
+//        p4.start();
 
         try{
-            p1.join();
-            p2.join();
-            p3.join();
-            p4.join();
+            for (int i = 0; i <threads; i++) {
+                p.get(i).join();
+            }
+//            p1.join();
+//            p2.join();
+//            p3.join();
+//            p4.join();
         } catch (InterruptedException e) {
             System.out.println("Thread interrupted\n"+e);
         }
 
-        System.out.println("Prime numbers from " + from + " to " + to);
-        soutPrimes();
+//        System.out.println("Prime numbers from " + from + " to " + to);
+//        soutPrimes();
 
-//        System.out.println(primeNumbers.size());
+        System.out.println(primeNumbers.size());
     }
 }
